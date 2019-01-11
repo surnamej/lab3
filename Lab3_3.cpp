9c6cb57901173a8c609078b1536fe7a79e037d0c
lab302-1 <lab302-1@labeng.local>
2019-01-11 17:13:36 +0700

    change number

--- Lab3_3.cpp
+++ Lab3_3.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    double x=2,sum=0;
-    while(x<=69)
+    double x=6,sum=0;
+    while(x<=19)
     {
         sum= sum+1/x;
         x++;
