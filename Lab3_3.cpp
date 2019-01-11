7eb1eac35578fadaaa17d8752385fc95a9045b73
lab302-1 <lab302-1@labeng.local>
2019-01-11 17:10:04 +0700

    Finish

--- Lab3_3.cpp
+++ Lab3_3.cpp
@@ -1,1 +1,14 @@
 #include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    double x=2,sum=0;
+    while(x<=69)
+    {
+        sum= sum+1/x;
+        x++;
+    }
+    cout<< sum;
+    return 0;
+}
